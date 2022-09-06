9557c2ec6e421621a09082b7524b2560241e69d5
moon350703@gmail.com <moon350703@gmail.com>
2022-09-06 17:41:06 +0700

    file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐 Hee loo ! 🖐<br>Bạn xinh gái',
+    introDesc: `Tớ có điều này muốn hỏi cậu, cậu nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Noel này cậu có muốn đi chơi với tớ khumm? 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu thích tớ đó nha :> ',
+    btnYes: 'Có, tớ sẽ đi chơi với cậu 💖💖💖',
+    btnNo: '💔 Cậu mơ à ! 💔 ',
+    question:'Cậu có gì mún nói với tớ hơm  😊😊',
+    reasonPlaceholder: 'Lời nhắn iu thương',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Ngày 25 này, cậu ăn xong rồi qua đón tớ đi chơi nha 😊😊',
+    mess: 'Tớ biết mà, hẹn cậu ngày 25 nhá . Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tớ nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/NgAnhTu873' 
+}
